05f0a8c4ef838959f437b2c81008490d7831882d
Fatimah Tanjeem Sabiha <87032057+FatimahTanjeemSabiha@users.noreply.github.com>
2022-09-29 00:36:27 -0700

    Add files via upload

---
+++ User Input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int num;
+    cout<< "Enter an integer number: ";
+    cin>> num;
+    cout<<"The entered number is: " << num;
+    getch();
+}
---
+++ Variable Declaration and Initialization.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int num1 = 25;
+    int num2 = 20;
+    float num3 = 50.35;
+    double num4 = 50.34739283;
+    char ch = 'a';
+    char ch1[7] = "Number";
+
+    cout <<num1<< endl <<num2<< endl;
+    cout <<num3<< endl;
+    cout <<num4<< endl;
+    cout << "The character is: "<<ch << endl;
+    cout << ch1;
+    getch();
+
+
+
+}
